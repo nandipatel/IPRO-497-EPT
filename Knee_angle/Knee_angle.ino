132fcf00401ce06497e79e0a5a90b693fe616711
Enrique Budiono <ebudiono@hawk.iit.edu>
2022-06-07 19:02:07 -0500

    adding new knee project

--- Knee_angle/Knee_angle.ino
+++ Knee_angle/Knee_angle.ino
@@ -1,9 +1,26 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <bluefruit.h>
 
+const int xInput = A0;
+const int yInput = A1;
+const int zInput = A2;
+
+int xRawMin = 512;
+int xRawMax = 512;
+
+int yRawMin = 512;
+int yRawMax = 512;
+
+int zRawMin = 512;
+int zRawMax = 512;
+
+const int sampleSize = 10;
+
+void setup()
+{
+  Serial.begin(9600);  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
 
 }
