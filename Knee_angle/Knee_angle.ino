973aca4c1da4bd823be0934ce3eb9b9922ea250b
Enrique Budiono <ebudiono@hawk.iit.edu>
2022-06-07 20:43:00 -0500

    initial accelometer

--- Knee_angle/Knee_angle.ino
+++ Knee_angle/Knee_angle.ino
@@ -4,14 +4,8 @@ const int xInput = A0;
 const int yInput = A1;
 const int zInput = A2;
 
-int xRawMin = 512;
-int xRawMax = 512;
-
-int yRawMin = 512;
-int yRawMax = 512;
-
-int zRawMin = 512;
-int zRawMax = 512;
+int RawMin = 0;
+int RawMax = 1023;
 
 const int sampleSize = 10;
 
@@ -22,5 +16,49 @@ void setup()
 
 void loop()
 {
+  // Read raw values
+  int xRaw = ReadAxis(xInput);
+  int yRaw = ReadAxis(yInput);
+  int zRaw = ReadAxis(yInput);
 
+  // Convert raw values to 'milli-Gs"
+  long xScaled = map(xRaw, RawMin, RawMax, -3000, 3000);
+  long yScaled = map(yRaw, RawMin, RawMax, -3000, 3000);
+  long zScaled = map(zRaw, RawMin, RawMax, -3000, 3000);
+
+  // re-scale to fractional Gs
+  float xAccel = xScaled / 1000.0;
+  float yAccel = yScaled / 1000.0;
+  float zAccel = zScaled / 1000.0;
+
+  Serial.print("X, Y, Z  :: ");
+  Serial.print(xRaw);
+  Serial.print(", ");
+  Serial.print(yRaw);
+  Serial.print(", ");
+  Serial.print(zRaw);
+  Serial.print(" :: ");
+  Serial.print(xAccel,0);
+  Serial.print("G, ");
+  Serial.print(yAccel,0);
+  Serial.print("G, ");
+  Serial.print(zAccel,0);
+  Serial.println("G");
+
+  delay(200);
+  system("cls");
+}
+
+int ReadAxis(int axisPin)
+{
+  long reading = 0;
+  analogRead(axisPin);
+  
+  delay(1);
+  
+  for(int i = 0; i < sampleSize; i++)
+  {
+    reading += analogRead(axisPin);
+  }
+  return reading / sampleSize;
 }
