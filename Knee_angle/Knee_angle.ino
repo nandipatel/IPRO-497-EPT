54f7aa0ede3bc6d82028c96c5ee38ae3ac8b980f
Enrique Budiono <ebudiono@hawk.iit.edu>
2022-06-08 18:41:56 -0500

    adding new code for the project

--- Knee_angle/Knee_angle.ino
+++ Knee_angle/Knee_angle.ino
@@ -1,4 +1,6 @@
 #include <bluefruit.h>
+#include <math.h>       /* atan2 */
+
 
 const int xInput = A0;
 const int yInput = A1;
@@ -9,44 +11,43 @@ int RawMax = 1023;
 
 const int sampleSize = 10;
 
+double roll = 0.00, pitch = 0.00, pitch2 = 0.00;
 void setup()
 {
-  Serial.begin(9600);  
+  Serial.begin(9600); 
+  Serial.println("Knee Brace Callibration test"); 
 }
 
 void loop()
 {
   // Read raw values
-  int xRaw = ReadAxis(xInput);
-  int yRaw = ReadAxis(yInput);
-  int zRaw = ReadAxis(yInput);
+  double xRaw = ReadAxis(xInput);
+  double yRaw = ReadAxis(yInput);
+  double zRaw = ReadAxis(yInput);
 
-  // Convert raw values to 'milli-Gs"
-  long xScaled = map(xRaw, RawMin, RawMax, -3000, 3000);
-  long yScaled = map(yRaw, RawMin, RawMax, -3000, 3000);
-  long zScaled = map(zRaw, RawMin, RawMax, -3000, 3000);
+  /* Convert raw values to 'milli-Gs"
+  long xScaled = map(xRaw, RawMin, RawMax, -100, 100);
+  long yScaled = map(yRaw, RawMin, RawMax, -100, 100);
+  long zScaled = map(zRaw, RawMin, RawMax, -100, 100);
 
   // re-scale to fractional Gs
   float xAccel = xScaled / 1000.0;
   float yAccel = yScaled / 1000.0;
-  float zAccel = zScaled / 1000.0;
+  float zAccel = zScaled / 1000.0; */
 
-  Serial.print("X, Y, Z  :: ");
-  Serial.print(xRaw);
-  Serial.print(", ");
-  Serial.print(yRaw);
-  Serial.print(", ");
-  Serial.print(zRaw);
-  Serial.print(" :: ");
-  Serial.print(xAccel,0);
-  Serial.print("G, ");
-  Serial.print(yAccel,0);
-  Serial.print("G, ");
-  Serial.print(zAccel,0);
-  Serial.println("G");
-
+  roll = atan2(yRaw, zRaw) * 57.3;
+  pitch = atan2((xRaw) , sqrt(yRaw * yRaw + zRaw * zRaw)) * 57.3;
+  pitch2 = atan2((yRaw) , sqrt(xRaw * xRaw + zRaw * zRaw)) * 57.3;
+  
+  
+  Serial.print(roll);
+  Serial.print(" deg,");
+  Serial.print(pitch);
+  Serial.print(" deg, ");
+  Serial.print(pitch2);
+  Serial.println(" deg");
+  
   delay(200);
-  system("cls");
 }
 
 int ReadAxis(int axisPin)
